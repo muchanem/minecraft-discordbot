7ba5b9f57b96fe6615bba0d36c06a50bfcc4a7f8
Mark Muchane <muchanem@gmail.com>
2019-07-09 06:31:19 -0400

    save

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 // discordbot code here
+
+const Discord = require('discord.js')
+const dotenv = require('dotenv');
+dotenv.config();
+const client = new Discord.Client()
+const token = process.env.DISCORD_BOT_SECRET
+
+client.on('ready', () => {
+  console.log('discordbot logged in')
+  console.log(client.user.username)
+})
+
+
+client.on('message', msg => {
+  
+  /*  if (msg.author.id != client.user.id) {
+        msg.channel.send(msg.content.split('').reverse().join(''))
+    }
+    if (msg.content == '!')
+    */
+})
+
+client.login(token)
